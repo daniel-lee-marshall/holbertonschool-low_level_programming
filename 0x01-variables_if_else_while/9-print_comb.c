2a55d2ca3e383d25e0593efd9a05b3cc3f689022
daniel-lee-marshall <daniel.lee.marshall83@gmail.com>
2021-02-05 05:30:14 +0000

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints out all the possible combos of single digit numbers
+ * Return: Prints 0
+*/
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+
+}
