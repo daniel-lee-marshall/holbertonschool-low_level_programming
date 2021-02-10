ed19fa2452facb9599b773307c7e4880847b4251
daniel-lee-marshall <daniel.lee.marshall83@gmail.com>
2021-02-10 04:17:03 +0000

    add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - will add the two integers together
+ * @i: 1st number
+ * @j: 2nd number
+ * Return: the sum of both integers
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
