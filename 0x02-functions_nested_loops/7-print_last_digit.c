9c660c4860a94dc8138c7f5aae77702ccefe9cdf
daniel-lee-marshall <daniel.lee.marshall83@gmail.com>
2021-02-10 02:53:31 +0000

    Add 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_last_digit - finds and prints the last digit of a number
+ * @number: the number to be checked
+ * Return: prints the last digit of the number
+ */
+int print_last_digit(int number)
+{
+	if (number < 0)
+	{
+		putchar('-');
+		number *= -1;
+	}
+}
