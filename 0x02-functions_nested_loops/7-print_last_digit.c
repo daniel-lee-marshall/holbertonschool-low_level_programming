34c3b48d1050c3c3c3cf1fd26a47ea9edd6e2d48
daniel-lee-marshall <daniel.lee.marshall83@gmail.com>
2021-02-10 03:15:28 +0000

    Add 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,14 @@
 /**
  * print_last_digit - finds and prints the last digit of a number
  * @n: the number to be checked
- * Return: prints the last digit of the number
+ * Return: prints the last digit of the number.
  */
 int print_last_digit(int n)
 {
 	if (n < 0)
 		n = n * -1;
+	{
 	_putchar((n % 10) + '0');
 	return (n % 10);
+	}
 }
