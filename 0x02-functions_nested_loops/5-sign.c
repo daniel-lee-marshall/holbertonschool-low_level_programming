82c0b5c430b363c55308e058e4463d16a8c2ffa1
daniel-lee-marshall <daniel.lee.marshall83@gmail.com>
2021-02-10 02:12:18 +0000

    Add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - tests if int is +,-, or 0
+ * @n: the number that is checked
+ * Return: 1 for +, 0 for zero, and -1 for -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
